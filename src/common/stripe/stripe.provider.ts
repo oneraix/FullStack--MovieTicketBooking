3da0128907757b452efbe549b37ed85289cfabce
Hieu <oneraix113@gmail.com>
2026-02-20 14:16:48 +0700

    change hard config to Dependence Injection config

--- src/common/stripe/stripe.provider.ts
+++ src/common/stripe/stripe.provider.ts
@@ -1,12 +1,18 @@
 import { STRIPE_SECRET_KEY } from '../constant/app.constant';
 import Stripe from 'stripe';
 import { STRIPE_CLIENT } from './stripe.constant';
+import { ConfigType } from '@nestjs/config';
+import stripeConfig from 'src/config/stripe.config';
+
 
 export const stripeProvider = {
   provide: STRIPE_CLIENT,
-  useFactory: () => {
-    return new Stripe(STRIPE_SECRET_KEY, {
+  useFactory: (config: ConfigType<typeof stripeConfig>) => {
+    return new Stripe(config.apiKey!, {
       apiVersion: '2025-07-30.basil',
+      typescript: true,
     });
   },
+
+  inject: [stripeConfig.KEY],
 };
---
+++ src/config/jwt.config.ts
@@ -0,0 +1,8 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('jwt',() =>({
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+    accessTokenExpires: process.env.ACCESS_TOKEN_EXPIRES,
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+    refreshTokenExpires: process.env.REFRESH_TOKEN_EXPIRES
+}))
